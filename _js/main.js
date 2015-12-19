f5c03e027d3c784f99bc08fccd0f34c02bc79d8e
zachalbert <zachalbert@gmail.com>
2015-12-18 16:18:26 -0800

    Initial commit

---
+++ _js/main.js
@@ -0,0 +1,26 @@
+// Put javascripty things here
+console.log('
+                  ____ _x***,  ____
+                j^   "wF     h#"  "q
+           w^""q#_   _0Np___g#L_   jLM"9*_
+          0     #NM*N0##"   N#N@**0#L    ]r
+      _*^*4p__gNN"    0h_   j#F    9Np__g0M^*s_
+     jF    B@^"90p___g##@^^*N#h____0@""@NF    0
+     _h___gF    jNP""9NF     #@"""N#     #___j@_
+   p"""N###Np__g0F    j#p,,gN#     Bp___d###@"""N_
+  t     @"  "NNNN#gggg#@"   5##ggggNNN#P   "L    I
+   9ggg0L   _F    0@^^N#_  _j@@^NN"   `L    #p_gp"
+  p"""N##ggg#L_  _f    j@""9N     L   j#NggN##@""^_
+ 4     #"  "0##*NN#___a&     p___g##*NN#F  "NF    0
+  ^u_gdL    JF    0#@"""-wgr"""N##"    #_   j#p_ar"
+   p"""0NpgNNL_  _0#L    j#     ##_   j##pggN@""N,
+  t_    ##"  "0NN@"""*gggN#Mggp^""9NNNF   N#L    I
+   9u__g##_   j##L    TNF"""N#     0##_   j##p_a*"
+     qF   9ggN@"""*gggNE     #Nggp^""9#gg@"  "b
+     `L    0##_    0N@^^#pgp@^@N#L    d##L   _0
+      "*wr4P"""qggg#     ]#     0Npgg^"""#*w*"
+          0_    #NN@Ns__g0#N___w@NN#L    jL
+           "*mm^B     0#"" "N#F    ]F*mmM"
+                "w___y^L    _Fh___a^
+                   ""   9***"   ""       
+                                   ProductDesignManu.al');
