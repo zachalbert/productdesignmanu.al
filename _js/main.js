2cbee5acad7b98c03116a8cc13a0f6b305be2a83
zachalbert <zachalbert@gmail.com>
2016-02-02 16:26:16 -0800

    This is a bad commit, with many changes

--- _js/main.js
+++ _js/main.js
@@ -2,4 +2,11 @@ $(document).ready( function() {
   // When a primary nav link is clicked...
   // use the href # to figure out which secondary nav to show
   // If it's not active, hide all and show the relevant ID
+  $('.primary-nav__link').click( function(e) {
+    e.preventDefault();
+    var id = $(this).attr('href');
+
+    $('.cat-list').removeClass('is-visible');
+    $(id).toggleClass('is-visible');
+  });
 });
