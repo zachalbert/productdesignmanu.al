32e255c1e0d04ae1bf487dd59dc02afb59ae4310
zachalbert <zachalbert@gmail.com>
2016-02-03 17:18:16 -0800

    Finally get primary + secondary nav working

--- _js/main.js
+++ _js/main.js
@@ -1,12 +1,45 @@
 $(document).ready( function() {
+
+  $('.cat-list.should-show').css('display', 'block');
+
   // When a primary nav link is clicked...
-  // use the href # to figure out which secondary nav to show
-  // If it's not active, hide all and show the relevant ID
   $('.primary-nav__link').click( function(e) {
     e.preventDefault();
-    var id = $(this).attr('href');
 
-    $('.cat-list').removeClass('is-visible');
-    $(id).toggleClass('is-visible');
+    // Only do stuff is the category isn't already selected
+    if( !$(this).hasClass( 'active' ) ) {
+
+      // Reassign the active class to the thing you just clicked
+      $('.primary-nav__link').removeClass( 'active' );
+      $(this).addClass( 'active' );
+
+      // targetList is the id of the list we want to be shown
+      var targetList = $(this).attr('href').substring(1); // e.g. typography
+
+      $('.cat-list').css('display', 'none');
+      $('#' + targetList).show();
+
+    };
+
+    return false;
   });
+
 });
+
+// Twitter asynch JS
+window.twttr = (function(d, s, id) {
+  var js, fjs = d.getElementsByTagName(s)[0],
+    t = window.twttr || {};
+  if (d.getElementById(id)) return t;
+  js = d.createElement(s);
+  js.id = id;
+  js.src = "https://platform.twitter.com/widgets.js";
+  fjs.parentNode.insertBefore(js, fjs);
+
+  t._e = [];
+  t.ready = function(f) {
+    t._e.push(f);
+  };
+
+  return t;
+}(document, "script", "twitter-wjs"));
